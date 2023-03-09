3a888ac898945c95579e0e1b5f147e6689e24f4e
Chiao52 <88649220+Chiao52@users.noreply.github.com>
2023-03-09 20:25:25 +0800

    Update connection.py

--- function/connection.py
+++ function/connection.py
@@ -3,14 +3,7 @@ import os
 import sys
 
 def ubuntu(ovpn_file_content, vpn_hostname, vpn_ip, vpn_country):
-
-    openvpn_path = input("【 Please input your openVPN software file path. 】\n\n E.g. /etc/openvpn \n\n=> ")
-    while(openvpn_path.strip() == ''):
-        print("[Sorry, this path information is necessary, please input again.]")
-        print('\n-----------------------------------\n')
-        openvpn_path = input("【 Please input your openVPN software file path. 】\n\n E.g. /etc/openvpn \n\n=> ")
-    
-    print('\n-----------------------------------\n')
+    openvpn_path = "/etc/openvpn"
     L_path = os.path.join(openvpn_path, 'client', 'vpngate_{}_{}_{}.ovpn'.format(vpn_hostname, vpn_country, vpn_ip))
     L_path = re.sub("\[|\]|\'","",L_path)
     with open(L_path, mode="w") as file:
@@ -28,14 +21,7 @@ def macos(ovpn_file_content, vpn_hostname, vpn_ip, vpn_country):
     print("\n## The vpngate_{}_{}_{}.ovpn file imported successfully. \n\n===== ## Now, you can connect with OpenVPN ! ===== \n\n".format(vpn_hostname, vpn_country, vpn_ip)) 
 
 def windows(ovpn_file_content, vpn_hostname, vpn_ip, vpn_country):
-
-    openvpn_path = input("【 Please input your openVPN software file path. 】\n\n E.g. /etc/openvpn \n\n=> ")    
-    while(openvpn_path.strip()==''): 
-        print("\n[Sorry, this path information is necessary, please input again.]")
-        print('\n-----------------------------------\n')
-        openvpn_path = input("【 Please input your openVPN software file path. 】\n\n E.g. /etc/openvpn \n\n=> ")
-
-    print('\n-----------------------------------\n')
+    openvpn_path = "/etc/openvpn"
     W_path = os.path.join(openvpn_path, 'config', 'vpngate_{}_{}_{}.ovpn'.format(vpn_hostname, vpn_country, vpn_ip))
     with open(W_path, mode="w") as file:
         file.write(ovpn_file_content)
